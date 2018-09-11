81b12d911c9d7eb0eea4fa53ae8fdf28c9c8c2b8
sohail ahmad <42468552+sohaileh@users.noreply.github.com>
2018-09-11 23:46:57 +0530

    Update save1.js

--- save1.js
+++ save1.js
@@ -10,13 +10,16 @@ var AWSDynamo = require("aws-sdk");
 //----------------------------------------------------------connection to s3---------------------------------------------------------
 var awsConfig = {
     "region": "us-east-2",
-    "endpoint": "http://s3.us-east-2.amazonaws.com"
-   
+    "endpoint": "http://s3.us-east-2.amazonaws.com",
+    "accessKeyId": "AKIAI42O4LALAFI5SBWA", "secretAccessKey": "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"
+};
 };
 //----------------------------------------------------------connection to DynamoDb---------------------------------------------------------
 let awsConfigDynamo = {
     "region": "us-east-2",
-    "endpoint": "http://dynamodb.us-east-2.amazonaws.com"
+    "endpoint": "http://dynamodb.us-east-2.amazonaws.com",
+     "accessKeyId": "AKIAI42O4LALAFI5SBWA", "secretAccessKey": "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"
+};
 };
 AWS.config.update(awsConfigDynamo);
 var docClient = new AWS.DynamoDB.DocumentClient();
