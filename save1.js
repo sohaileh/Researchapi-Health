1b269b06b0cf63e3f0d04d743c84ee2e2152d576
sohail ahmad <42468552+sohaileh@users.noreply.github.com>
2018-09-11 16:09:34 +0530

    Update save1.js

--- save1.js
+++ save1.js
@@ -142,51 +142,7 @@ function getawsurl(myKey, title, url) {
     var awsurl = s3.getSignedUrl('getObject', params);
     //console.log(`the url is${awsurl}`);
     saveDynamodb(awsurl, title, url);
-
 }
-////using get route
-app.get('/Researchapi/Health', function (req, res) {
-    //console.log(req.query["keyword"]);
-
-    FetchFromFile(res, req.query);
-})
-
-
-//fething function
-function FetchFromFile(res, query) {
-
-    var search = query["Searchkey"];
-    var modifieddateT = query["modifieddate"];
-    var allDataset = [];
-    fs.readFile('./fulldata.json', 'utf8', function (err, data) {
-        if (err) throw err;
-        allData = JSON.parse(data);
-
-        var rows = allData.dataset;
-        var issearched = false;
-        for (var index = 0; index < rows.length; index++) {
-            var row = rows[index];
-            if (search != "" && search != undefined) {
-                for (var i = 0; i < row.keyword.length; i++) {
-                    if (row.keyword[i].toLowerCase().indexOf(search.toLowerCase()) !== -1) {
-                        issearched = true;
-                    }
-                }
-                if (row.title.toLowerCase().indexOf(search.toLowerCase()) !== -1 || row.modified == modifieddateT) {
-                    issearched = true;
-                }
-                if (issearched)
-                    allDataset.push(row);
-                issearched = false;
-            }
-            else {
-                allDataset.push(row);
-            }
-        }
-        res.status(200).send(allDataset);
-    });
-}
-
 //------------------------------------------------Passing url to DynamoDb---------------------------------------------------
 let saveDynamodb = function (awsurl, title, url) {
     AWS.config.update(awsConfigDynamo);
